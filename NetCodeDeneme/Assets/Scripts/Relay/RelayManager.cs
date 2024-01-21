77d3fc84cce03e852c5839be2d0f9bf54bcd2154
Onur Küçük <124556360+OnurKck8@users.noreply.github.com>
2024-01-21 12:57:23 +0300

    Relay Joint Eklendi

--- NetCodeDeneme/Assets/Scripts/Relay/RelayManager.cs
+++ NetCodeDeneme/Assets/Scripts/Relay/RelayManager.cs
@@ -8,8 +8,13 @@ using Unity.Services.Relay.Models;
 public class RelayManager : MonoBehaviour
 {
     private string playerID;
-    private RelayHostData data;
+
+    private RelayHostData hostData;
+    private RelayJoinData joinData;
+
+    public TMP_InputField InputField;
     public TextMeshProUGUI IdText;
+    public TextMeshProUGUI joinCodeText;
     public TMP_Dropdown playerCount;
     async void Start()
     {
@@ -31,7 +36,7 @@ public class RelayManager : MonoBehaviour
     {
         int maxPLayerCount = Convert.ToInt32(playerCount.options[playerCount.value].text);
         Allocation allocation = await Unity.Services.Relay.RelayService.Instance.CreateAllocationAsync(maxPLayerCount);
-        data = new RelayHostData()
+        hostData = new RelayHostData()
         {
             Ipv4Address = allocation.RelayServer.IpV4,
             port = (ushort)allocation.RelayServer.Port,
@@ -39,8 +44,28 @@ public class RelayManager : MonoBehaviour
             AllocationIDBytes = allocation.ConnectionData,
             Key = allocation.Key,
         };
+        hostData.JoinCode = await Unity.Services.Relay.RelayService.Instance.GetJoinCodeAsync(hostData.AllocationID);
+        Debug.Log("Allocation Complete" + hostData.AllocationID);
+        Debug.LogWarning("Join Code:" + hostData.JoinCode);
+        joinCodeText.text = hostData.JoinCode;
+    }
 
-        Debug.Log("Allocation Complete" + data.AllocationID);
+    public async void OnJoinClick()
+    {
+        JoinAllocation joinAllocation = await Unity.Services.Relay.RelayService.Instance.JoinAllocationAsync(InputField.text);
+
+        joinData = new RelayJoinData()
+        {
+            Ipv4Address = joinAllocation.RelayServer.IpV4,
+            port = (ushort)joinAllocation.RelayServer.Port,
+            AllocationID = joinAllocation.AllocationId,
+            AllocationIDBytes = joinAllocation.ConnectionData,
+            ConnectionData=joinAllocation.ConnectionData,
+            HostConnectionData=joinAllocation.HostConnectionData,
+            Key = joinAllocation.Key,
+        };
+        Debug.Log("Join Success" + joinData.AllocationID);
+        
     }
 }
 
@@ -52,5 +77,16 @@ public struct RelayHostData
     public Guid AllocationID;
     public byte[] AllocationIDBytes;
     public byte[] ConnectionData;
+    public byte[] Key;
+}
+
+public struct RelayJoinData
+{  
+    public string Ipv4Address;
+    public ushort port;
+    public Guid AllocationID;
+    public byte[] AllocationIDBytes;
+    public byte[] ConnectionData;
+    public byte[] HostConnectionData;
     public byte[] Key;
 }
